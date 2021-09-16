221905f005660815288d9188e35ccbf0e3a3f8de
Sambasiva Andaluri <andaluri@gmail.com>
2021-09-16 09:11:39 -0400

    tweaks

--- dev-test-run.sh
+++ dev-test-run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-mpdev verify --deployer=gcr.io/gke-istio-test-psb/tsb-operator/deployer:1.4 --parameters='{"name": "tsb", "namespace": "tsb","reportingSecret": "gs://cloud-marketplace-tools/reporting_secrets/fake_reporting_secret.yaml"}' | tee verify.log
+kubectl create ns tsb
+mpdev install --deployer=gcr.io/gke-istio-test-psb/tsb-operator/deployer:1.4 --parameters='{"name": "tsb", "namespace": "tsb","reportingSecret": "gs://cloud-marketplace-tools/reporting_secrets/fake_reporting_secret.yaml"}' | tee install.log
